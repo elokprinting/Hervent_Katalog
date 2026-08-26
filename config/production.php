c4df2711acf192837867dba3cc1d253df989bf09
ZDHAN-Maker <uprise419@gmail.com>
2026-08-26 10:42:04 +0700

    testing

--- config/production.php
+++ config/production.php
@@ -5,5 +5,5 @@ return [
     | Password hash for the production content area. Generate it with:
     | php artisan tinker --execute="echo Hash::make('a-long-unique-password');"
     */
-    'password_hash' => env('PRODUCTION_PASSWORD_HASH'),
+    'password_hash' => env('Hervent2026'),
 ];
