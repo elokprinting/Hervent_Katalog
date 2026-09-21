dec85cc0d913e0d6a825f7d336352faa408c356b
ZDHAN-Maker <uprise419@gmail.com>
2026-09-21 14:46:16 +0700

    restore seminar kit landing route

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/lang/{locale}', [LanguageController::class, 'switchLang'])->name('lang.switch');
 
 Route::get('/', HomeController::class)->name('home');
-Route::view('/landing-page-paket-seminar-kit-paling-kekinian', 'landing.corporate-gift')->name('landing.corporate-gift.seminar-alias');
+Route::view('/landing-page-paket-seminar-kit-paling-kekinian', 'landing.seminar-kit')->name('landing.seminar-kit');
 Route::view('/lp-paket-souvenir-paket-merchandise-corporate-gift-set-kantor-custom', 'landing.corporate-gift')->name('landing.corporate-gift');
 Route::view('/souvenir-kantor-perusahaan-corporate-gift-premium-eksklusif-acara-kekinian', 'landing.corporate-gift')->name('landing.corporate-gift.souvenir-alias');
 Route::view('/about', 'about')->name('about');
